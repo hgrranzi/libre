5c4d80b34f2fda09f9ef2f1a738aef48b8fc119c
hgrranzi <vkh.mariia@gmail.com>
2021-04-21 19:43:24 +0300

    Created a header for some recoded ctype functions

---
+++ srcs/aka_ctype.h
@@ -0,0 +1,13 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+#ifndef AKA_CTYPE_H
+# define AKA_CTYPE_H
+
+int	aka_isalpha(int c);
+int	aka_isdigit(int c);
+
+#endif
