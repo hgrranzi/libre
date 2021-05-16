c0be47df3a98624c81f7635646269a1b579f42ae
hgrranzi <vkh.mariia@gmail.com>
2021-05-16 13:47:01 +0300

    Created the aka_strcmp function

---
+++ srcs/aka_strcmp.c
@@ -0,0 +1,15 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+int	aka_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
---
+++ srcs/aka_string.h
@@ -0,0 +1,12 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+#ifndef AKA_STRING_H
+# define AKA_STRING_H
+
+int	aka_strcmp(const char *s1, const char *s2);
+
+#endif
